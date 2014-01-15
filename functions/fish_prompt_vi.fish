b5996f29abd8202ebe7e0e1a2f7dc746c5f8c0d7
Maxim Gonchar <gmaxfl@gmail.com>
2014-01-15 18:07:34 +0800

    Update curosr

--- functions/fish_prompt_vi.fish
+++ functions/fish_prompt_vi.fish
@@ -1,14 +1,23 @@
+set -g fish_cursor_default
+set -g fish_cursor_insert
+
+if set -q KONSOLE_PROFILE_NAME
+    set fish_cursor_insert "\e]50;CursorShape=1\x7"
+    set fish_cursor_default "\e]50;CursorShape=0\x7"
+end
+
 if type bind_mode >/dev/null
     function fish_prompt_vi
         switch (bind_mode)
             case insert
                 echo -n "$fish_color_mode_insert""[i] "
+                echo -en $fish_cursor_insert
             case default
                 echo -n "$fish_color_mode_default""[n] "
-            case command
-                echo -n "$fish_color_mode_command""[c] "
+                echo -en $fish_cursor_default
             case '*'
-                echo -n "["(bind_mode)"] "
+                echo -n $fish_color_mode_default"["(bind_mode)"] "
+                echo -en $fish_cursor_default
         end
     end
 else
