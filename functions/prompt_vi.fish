49cd6026340f101f56e0489e74e83eca60cb42c4
Maxim Gonchar <gmaxfl@gmail.com>
2014-01-30 14:04:04 +0400

    misc

--- functions/prompt_vi.fish
+++ functions/prompt_vi.fish
@@ -1,15 +1,16 @@
 function prompt_vi --argument pre post
-	set -q post[1]; or set post "]"
+	set -q post[1]
+	or set post "]"
 	switch "$fish_bind_mode"
-        case insert
-            echo -n "$fish_color_mode_insert""$pre"i"$post"" "
-        case default
-            echo -n "$fish_color_mode_default""$pre"n"$post"" "
-        case visual
-            echo -n "$fish_color_mode_visual""$pre"v"$post"" "
-        case ''
-            echo -n ''
-        case '*'
-            echo -n "$fish_color_mode_default""$pre"$fish_bind_mode"$post"" "
-    end
+	case insert
+		echo -n "$fish_color_mode_insert""$pre"i"$post"
+	case default
+		echo -n "$fish_color_mode_default""$pre"n"$post"
+	case visual
+		echo -n "$fish_color_mode_visual""$pre"v"$post"
+	case ''
+		echo -n ''
+	case '*'
+		echo -n "$fish_color_mode_default""$pre"$fish_bind_mode"$post"
+	end
 end
