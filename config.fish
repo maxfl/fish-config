bc411eeb31df37b63a9ef55c21bc73d93bde8a24
Maxim Gonchar <gmaxfl@gmail.com>
2014-03-31 12:18:10 +0400

    misc

--- config.fish
+++ config.fish
@@ -1,14 +1,8 @@
-setfirstexecutable EDITOR vim joe mcedit nano pico
-setfirstexecutable BROWSER opera chromium luakit firefox iceweasel
-setfirstexecutable PAGER vimpager most less more
-
 if status --is-interactive
     if test "$TERM" = rxvt-unicode-256color
         set -lx TERM xterm-256color
-        set -x LS_COLORS (sh -c 'eval $(dircolors -b); echo $LS_COLORS | tr : \\\n')
-    else
-        set -x LS_COLORS (sh -c 'eval $(dircolors -b); echo $LS_COLORS | tr : \\\n')
     end
+    set -x LS_COLORS (sh -c 'eval $(dircolors -b); echo $LS_COLORS | tr : \\\n')
 
     if not set -Uq fish_user_config_set
         set_user_colors
--- functions/tvim.fish
+++
@@ -1,3 +0,0 @@
-function tvim --description 'Run gvim in tmux session'
-	tmux new-session "gvim -f $argv"
-end
