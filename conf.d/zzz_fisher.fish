827b73a7f2337ce4f36befa8e4e9a74d204dd10d
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2019-10-23 22:43:25 +0300

    update

--- conf.d/zzz_fisher.fish
+++ conf.d/zzz_fisher.fish
@@ -1,6 +1,6 @@
 set -g fisher_path ~/.config/fish/packages
 
-set --prepend fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
+set --prepend fish_function_path $fish_function_path[1] $fisher_path/functions ~/.config/fish/functions_backup $fish_function_path[2..-1]
 set --prepend fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
 
 for file in $fisher_path/conf.d/*.fish
--- functions/screen.fish
+++
@@ -1,4 +0,0 @@
-function screen --description 'Call screen with smaller TERM variable'
-	set -lx TERM rxvt-256color
-    command screen $argv
-end
