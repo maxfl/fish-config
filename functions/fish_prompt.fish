0a0ed75c4dba366a5dd6b91ebd886a630dfb3996
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2019-10-23 16:52:51 +0300

    minor

---
+++ functions/fish_prompt.fish
@@ -0,0 +1,31 @@
+# Defined in /tmp/fish_funced.VjYxilwMCL.fish @ line 2
+function fish_prompt --description 'Write out the prompt'
+	set -l __fish_prompt_last_status $status
+
+	if not set -q __fish_prompt_normal
+		set -g __fish_prompt_normal (set_color normal)
+	end
+
+	set -l suffix '% '
+
+	#echo -ne '\033[2K'
+    if functions -q fish_vi_cursor_handle
+        fish_vi_cursor_handle
+    end
+	#prompt_vi '[%s] '
+    fish_prompt_label
+    fish_prompt_userhost
+	echo -n "$fish_prompt_normal":
+	prompt_pwd
+    echo -n "$fish_prompt_normal"
+    prompt_history
+    prompt_git
+	prompt_njobs
+	prompt_last_status
+	prompt_date
+    echo -n "$fish_prompt_normal"
+    if test "$fish_prompt_multiline"
+        echo
+    end
+	echo -n $suffix
+end
