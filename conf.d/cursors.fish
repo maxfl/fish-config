8e7969c8fd0a87a96c020320c5026f7600efedd2
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2019-10-25 10:06:23 +0300

    minor

---
+++ conf.d/cursors.fish
@@ -0,0 +1,5 @@
+set -g fish_cursor_insert  line
+set -g fish_cursor_default block
+set -g fish_cursor_visual  underscore
+set -g fish_cursor_unknown block blink
+
--- conf.d/spacefish.fish
+++ conf.d/spacefish.fish
@@ -1,18 +1,23 @@
+
+# PROMPT
 #set -g SPACEFISH_PROMPT_ORDER vi_mode user dir host git package node exec_time jobs time line_sep exit_code char
 set -g SPACEFISH_PROMPT_ORDER user dir host git package node exec_time jobs time line_sep exit_code char_vi
+set -g SPACEFISH_PROMPT_ADD_NEWLINE false
+set -g SPACEFISH_PROMPT_PREFIXES_SHOW false
+
+# CHAR
 if not set -q SPACEFISH_CHAR_SYMBOL
     set -g SPACEFISH_CHAR_SYMBOL '❯'
 end
-set -g SPACEFISH_DIR_PREFIX ''
+# DIR
 set -g SPACEFISH_DIR_TRUNC_REPO false
-set -g SPACEFISH_GIT_PREFIX ''
+# GIT
 set -g SPACEFISH_GIT_SYMBOL ''
-set -g SPACEFISH_HOST_PREFIX ''
-set -g SPACEFISH_PROMPT_ADD_NEWLINE false
+# TIME
 set -g SPACEFISH_TIME_FORMAT '%a %H:%M:%S'
-set -g SPACEFISH_TIME_PREFIX ''
 set -g SPACEFISH_TIME_SHOW true
-set -g SPACEFISH_USER_PREFIX ''
+set -g SPACEFISH_TIME_COLOR brgrey
+# VI mode
 set -g SPACEFISH_VI_MODE_INSERT (set_color normal)I
 set -g SPACEFISH_VI_MODE_NORMAL N
 set -g SPACEFISH_VI_MODE_REPLACE_ONE (set_color red)R
