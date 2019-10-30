23abab96f9b9afbe21aa0d872b1f4c63a026037e
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2019-10-30 14:10:18 +0300

    cleaning

--- packages/functions/__sf_section_char_vi.fish
+++ packages/functions/__sf_section_char_vi.fish
@@ -2,33 +2,31 @@
 # Prompt character
 #
 
-function __sf_section_char_vi -d "Display the prompt character"
+function __sf_section_char_vi -d "Display the prompt character and vi mode status"
 	# ------------------------------------------------------------------------------
 	# Configuration
 	# ------------------------------------------------------------------------------
 
 	__sf_util_set_default SPACEFISH_CHAR_VI_PREFIX ""
 	__sf_util_set_default SPACEFISH_CHAR_VI_SUFFIX " "
-	__sf_util_set_default SPACEFISH_CHAR_VI_SYMBOL "❯"
+	__sf_util_set_default SPACEFISH_CHAR_VI_SYMBOL "➜"
 	__sf_util_set_default SPACEFISH_CHAR_VI_COLOR_SUCCESS     normal
-    __sf_util_set_default SPACEFISH_CHAR_VI_COLOR_FAILURE     --background red
+    __sf_util_set_default SPACEFISH_CHAR_VI_COLOR_FAILURE     red -r
     __sf_util_set_default SPACEFISH_CHAR_VI_COLOR_INSERT      normal
-    __sf_util_set_default SPACEFISH_CHAR_VI_COLOR_NORMAL      cyan
-    __sf_util_set_default SPACEFISH_CHAR_VI_COLOR_REPLACE_ONE red
-    __sf_util_set_default SPACEFISH_CHAR_VI_COLOR_VISUAL      yellow
+    __sf_util_set_default SPACEFISH_CHAR_VI_COLOR_NORMAL      --background blue
+    __sf_util_set_default SPACEFISH_CHAR_VI_COLOR_REPLACE_ONE --background magenta
+    __sf_util_set_default SPACEFISH_CHAR_VI_COLOR_VISUAL      --background yellow
 
 	# ------------------------------------------------------------------------------
 	# Section
 	# ------------------------------------------------------------------------------
 
-	# Color $SPACEFISH_CHAR_VI_SYMBOL red if previous command failed and
-	# color it in green if the command succeeded.
 	set -l color
 
 	if test $sf_exit_code -eq 0
-		set color $SPACEFISH_CHAR_VI_COLOR_SUCCESS
+		set --append color $SPACEFISH_CHAR_VI_COLOR_SUCCESS
 	else
-		set color $SPACEFISH_CHAR_VI_COLOR_FAILURE
+		set --append color $SPACEFISH_CHAR_VI_COLOR_FAILURE
 	end
 
 	switch $fish_bind_mode
@@ -41,10 +39,9 @@ function __sf_section_char_vi -d "Display the prompt character"
 		case visual
 			set --append color $SPACEFISH_CHAR_VI_COLOR_VISUAL
 	end
-    set color (set_color $color)
 
 	__sf_lib_section \
-		$color \
+		"$color" \
 		$SPACEFISH_CHAR_VI_PREFIX \
 		$SPACEFISH_CHAR_VI_SYMBOL \
 		$SPACEFISH_CHAR_VI_SUFFIX
