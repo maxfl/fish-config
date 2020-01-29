727ef04299cbd6ba79effc8eb15820df65f9b3ff
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2020-01-29 12:44:00 +0300

    updates

--- conf.d/display.fish
+++ conf.d/display.fish
@@ -7,10 +7,15 @@ if set -q DISPLAY
     if set -q DISPLAY[2]
         set test $DISPLAY[1]:$DISPLAY[2]
     end
-    if not xdpyinfo -display "$test" >/dev/null ^/dev/null
-        set_color red
-        echo "Removing invalid display '$test'"
-        set_color normal
-        set -e DISPLAY
+
+    if type -q xdpyinfo
+        if not xdpyinfo -display "$test" >/dev/null ^/dev/null
+            set_color red
+            echo "Removing invalid display '$test'"
+            set_color normal
+            set -e DISPLAY
+        end
+    else
+        echo 'Install xdypinfo to test DISPLAY validity'
     end
 end
