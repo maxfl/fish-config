b7003e02fb7ce3197655ff995c173816223f8e85
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2020-06-04 07:57:40 +0300

    minor

--- packages_local/cmake_helpers/functions/cmake-mkbuild.fish
+++ packages_local/cmake_helpers/functions/cmake-mkbuild.fish
@@ -1,7 +1,17 @@
 function cmake-mkbuild -d 'Prepare new cmake build folder' -a path
     set -e argv[1]
+
+    if not test "$path"
+        echo 'Should provide valid path'
+        return 1
+    end
+
     set -l wd $PWD
-    pushd (realpath $path)
+    if not pushd (realpath $path)
+        popd
+        echo 'Should provide valid path'
+        return 1
+    end
 
     if count * > /dev/null
         command rm -rf *
