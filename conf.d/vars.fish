d31ecb9d750cc3b83fcd3c084089117becb9db95
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2021-02-09 13:17:10 +0300

    minor

--- conf.d/vars.fish
+++ conf.d/vars.fish
@@ -1,3 +1,3 @@
 type -fq less;                       and set -gx PAGER   less
 type -fq vivaldi-stable;             and set -gx BROWSER vivaldi-stable
-type -fq nvr; and set -q VIMRUNTIME; and set -gx EDITOR  nvr --remote-wait
+#type -fq nvr; and set -q VIMRUNTIME; and set -gx EDITOR  nvr --remote-wait
--- packages/completions/__gitnow_completions.fish
+++ packages/completions/__gitnow_completions.fish
@@ -1,5 +1,21 @@
 source $__fish_data_dir/completions/git.fish
 
+# Merge command
+
+complete -f -x -c merge -a '(__fish_git_branches)'
+
+complete -f -x -c merge \
+    -s h -l help \
+    -d "Show information about the options for this command"
+
+complete -f -x -c merge \
+    -s a -l abort \
+    -d "Abort conflicted merge"
+
+complete -f -x -c merge \
+    -s c -l continue \
+    -d "Continue merge"
+
 # Move command
 
 complete -f -x -c move -a '(__fish_git_branches)'
@@ -11,7 +27,7 @@ complete -f -x -c move \
 complete -f -x -c move \
     -s n -l no-apply-stash \
     -a '(__fish_git_branches)' \
-    -d "Switch to a local branch but without applying current stash" 
+    -d "Switch to a local branch but without applying current stash"
 
 complete -f -x -c move \
     -s u -l upstream \
@@ -21,6 +37,9 @@ complete -f -x -c move \
 
 # Tag command
 
+complete -f -x -c tag \
+    -d "List all tags in a lexicographic order and treating tag names as versions"
+
 complete -f -x -c tag -a '(__fish_git_tags)'
 
 complete -f -x -c tag \
@@ -29,7 +48,7 @@ complete -f -x -c tag \
 
 complete -f -x -c tag \
     -s l -l latest \
-    -d "Show the latest tag version"
+    -d "Show only the latest Semver release tag version (no suffixed ones or others)"
 
 complete -f -x -c tag \
     -s x -l major \
@@ -43,14 +62,15 @@ complete -f -x -c tag \
     -s z -l patch \
     -d "Tag auto-incrementing a patch version number"
 
-complete -f -x -c tag \
-    -s a -l premajor \
-    -d "Tag auto-incrementing a premajor version number"
+# TODO: pre-release versions are not supported yet
+# complete -f -x -c tag \
+#     -s a -l premajor \
+#     -d "Tag auto-incrementing a premajor version number"
 
-complete -f -x -c tag \
-    -s b -l preminor \
-    -d "Tag auto-incrementing a preminor version number"
+# complete -f -x -c tag \
+#     -s b -l preminor \
+#     -d "Tag auto-incrementing a preminor version number"
 
-complete -f -x -c tag \
-    -s c -l prepatch \
-    -d "Tag auto-incrementing a prepatch version number"
+# complete -f -x -c tag \
+#     -s c -l prepatch \
+#     -d "Tag auto-incrementing a prepatch version number"
