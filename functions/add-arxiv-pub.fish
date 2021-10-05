d1aa2c2a7cdd36d6ffefadb209543cfaa7a089ee
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2021-10-05 16:51:58 +0300

    minor

--- functions/add-arxiv-pub.fish
+++ functions/add-arxiv-pub.fish
@@ -1,5 +1,5 @@
 function add-arxiv-pub --description 'Copy publication from arxiv'
-    argparse --min-args 3 'tbd' -- $argv
+    argparse --min-args 3 'tbd' 'p/publication=' 'j/journal=' -- $argv
     or begin
         echo Invalid commandline: $argv
         return 1
@@ -12,7 +12,17 @@ function add-arxiv-pub --description 'Copy publication from arxiv'
     set -l newname (string join _ $exp $basename $argv).pdf
 
     echo $filename '->' $newname
-    if mv $filename $newname
+    if mv -vi $filename $newname
         add-bib-entry $newname $_flag_tbd
+        echo -n $newname | xsel
+    end
+
+    if begin
+        set -q _flag_publication
+        and set -q _flag_journal
+    end
+        set -l pubname (string join _ $exp $basename.$_flag_journal $argv).pdf
+        mv -vi $_flag_publication $pubname
+        echo -n $pubname | xsel
     end
 end
