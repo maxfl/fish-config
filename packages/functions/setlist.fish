bb12ca5d69f119aa1338934875901b3cfcbf1382
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2021-11-24 09:43:44 +0300

    update

--- packages/functions/setlist.fish
+++ packages/functions/setlist.fish
@@ -1,1 +1,16 @@
-/home/gonchar/.config/fish/packages_local/setlist/functions/setlist.fish
+function setlist -d 'Operation on variable lists' -S -a method var
+    set -e argv[1]
+    switch "$method"
+        case pop
+            __setlist_pop $argv
+
+        case rm-value
+            __setlist_rm_value $argv
+
+        case '*'
+            set_color red
+            echo "Unsupported method '$method'"
+            set_color normal
+            return 1
+    end
+end
